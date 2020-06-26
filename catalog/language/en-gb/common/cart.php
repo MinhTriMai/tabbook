bb1b6ba654ea0c310072c2442af24029fb6cce6d
Nhan Vo <nhan@dev.(none)>
2020-06-26 09:21:49 +0700

    add code to restore template

--- catalog/language/en-gb/common/cart.php
+++ catalog/language/en-gb/common/cart.php
@@ -2,9 +2,6 @@
 // Text
 $_['text_items']     = '%s';
 $_['text_items2']     = 'My cart';
-
-
-$_['text_shoppingcart']     = 'Shopping Cart';
 $_['text_empty']     = 'Your shopping cart is empty!';
 $_['text_cart']      = 'View Cart';
 $_['text_checkout']  = 'Checkout';
