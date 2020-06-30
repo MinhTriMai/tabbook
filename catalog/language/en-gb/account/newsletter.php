666771c5d2398208f15e842c98cf941f9de5a391
MinhTriMai <elliotdots1@gmail.com>
2020-06-30 23:26:57 +0700

    update language

--- catalog/language/en-gb/account/newsletter.php
+++ catalog/language/en-gb/account/newsletter.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Đăng kí nhận thông báo';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_account']     = 'Tài khoản';
+$_['text_newsletter']  = 'Thư thông báo';
+$_['text_success']     = 'Thành công: Bạn đã đăng kí nhận thư thông báo!';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe';
+$_['entry_newsletter'] = 'Đăng kí:';
---
+++ catalog/language/vi-vn/extension/module/newslettersubscribe.php
@@ -0,0 +1,30 @@
+<?php
+// Heading 
+$_['heading_title'] 	 = 'Bản tin';
+$_['heading_title2'] 	 = 'Đăng ký để <span>gửi bản tin</span>';
+$_['heading_title3'] 	 = 'Ưu đãi đặc biệt cho người đăng ký';
+$_['text_colose'] 	 = 'Đóng';
+
+$_['newletter_des'] 	 = 'Theo dõi bản tin của chúng tôi ngay bây giờ và cập nhật các quyển sách mới, tin tức mới nhất và cung cấp độc quyền bởi Tabbook.';
+$_['newletter_des2'] 	 = 'You can be always up to date with our company new!';
+
+//Fields
+$_['entry_email'] 		 = 'Nhập địa chỉ email của bạn ...';
+$_['entry_name'] 		 = 'Tên';
+
+//Buttons
+$_['entry_button'] 		 = 'Đăng ký';
+$_['entry_unbutton'] 	 = 'Hủy đăng ký';
+
+//text
+$_['text_subscribe'] 	 = 'Subscribe Here';
+
+$_['mail_subject']   	 = 'NewsLetter Subscribe';
+
+//Messages
+$_['error_invalid'] 	 = 'Lỗi : Hãy nhập địa chỉ email chính xác';
+$_['subscribe']	    	 = 'Thành công : Đăng ký thành công.';
+$_['unsubscribe'] 	     = 'Thành công : Hủy đăng ký thành công';
+$_['alreadyexist'] 	     = 'Lỗi : Email đã tồn tại.';
+$_['notexist'] 	    	 = 'Lỗi : Email không tồn tại.';
+$_['entry_show_again']   =  "Không hiện thông báo này nữa";
