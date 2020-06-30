e751cee358d70608fcbbf1c7c08b0c59e02e6dfc
MinhTriMai <elliotdots1@gmail.com>
2020-06-30 22:13:08 +0700

    update title last news

--- catalog/language/en-gb/blog/blog.php
+++ catalog/language/en-gb/blog/blog.php
@@ -3,6 +3,6 @@
 $_['button_read_more']         		 = 'Read more';
 $_['text_blog_empty']           			 = 'No articles';
 $_['text_post_by']           			 = 'post by: ';
-$_['text_headingtitle']           			 = 'LATEST NEWS';
+$_['text_headingtitle']           			 = 'TIN MỚI NHẤT';
 $_['text_blog'] = 'Blog';
 $_['sub_title'] = 'Our blog';
