8624679081f32a366ac25104092629572df70f8d
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-08-10 19:32:31 +0700

    add logic if not customer then appear menu in header

---
+++ catalog/language/vi-vn/extension/module/ochozmegamenu.php
@@ -0,0 +1,10 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Hozmegamenu';
+$_['category_title'] = 'Danh mục';
+$_['menu_title'] = 'Menu';
+
+$_['txt_become_seller'] = 'Trở thành người bán';
+
+// Text
+$_['text_reviews']  = 'Based on %s reviews.';
