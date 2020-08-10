37ba9887f705eea5913d24f572b66c3515fcac5e
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-08-10 19:35:57 +0700

    fix message order

--- catalog/language/vi-vn/checkout/success.php
+++ catalog/language/vi-vn/checkout/success.php
@@ -6,5 +6,6 @@ $_['heading_title']        = 'Đơn đặt hàng của bạn đã được đặ
 $_['text_basket']   	= 'Giỏ hàng';
 $_['text_checkout'] 	= 'Thanh toán';
 $_['text_success']  	= 'Thành công';
-$_['text_customer'] 	= '<p>Đơn hàng của bạn đã được xử lý thành công!</p><p>Bạn có thể xem lịch sử đơn hàng bằng cách vào trang <a href="%s">Tài khoản</a> và bấm vào liên kết <a href="%s">Lịch sử</a>.</p>Nếu bạn thanh toán để tải tập tin, bạn có thể vào trang <a href="%s">Tải tập tin</a> để xem.<p></p><p>Vui lòng gửi câu hỏi đến <a href="%s">quản lý gian hàng</a>.</p><p>Cám ơn bạn đã giao dịch với chúng tôi!</p>';
+// $_['text_customer'] 	= '<p>Đơn hàng của bạn đã được xử lý thành công!</p><p>Bạn có thể xem lịch sử đơn hàng bằng cách vào trang <a href="%s">Tài khoản</a> và bấm vào liên kết <a href="%s">Lịch sử</a>.</p>Nếu bạn thanh toán để tải tập tin, bạn có thể vào trang <a href="%s">Tải tập tin</a> để xem.<p></p><p>Vui lòng gửi câu hỏi đến <a href="%s">quản lý gian hàng</a>.</p><p>Cám ơn bạn đã giao dịch với chúng tôi!</p>';
+$_['text_customer'] 	= '<p>Đơn đặt hàng của bạn đã thành công. Bạn vui lòng kiểm tra email để nhận thông tin của người bán!</p>';
 $_['text_guest']    	= '<p>Đơn hàng của bạn đã được xử lý thành công!</p><p>Nếu có bất kỳ thắc mắc gì, vui lòng chuyển cho <a href="%s">chúng tôi</a>.</p><p>Cám ơn đã mua hàng tại cửa hàng online của chúng tôi!</p>';
