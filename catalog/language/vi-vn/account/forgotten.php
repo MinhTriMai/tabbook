d179235dc74ad632e92d6038d32870a21e5e0f40
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-08-22 19:23:17 +0700

    change forgot pasword text sucess

--- catalog/language/vi-vn/account/forgotten.php
+++ catalog/language/vi-vn/account/forgotten.php
@@ -7,7 +7,7 @@ $_['text_account']    = 'Tài Khoản';
 $_['text_forgotten']  = 'Quên Mật Khẩu';
 $_['text_your_email'] = 'Địa chỉ E-Mail của bạn';
 $_['text_email']      = 'Nhập địa chỉ email đã đăng kí với tài khoản này. Bấm "Tiếp tục", mật khẩu sẽ được gửi đến email của bạn';
-$_['text_success']    = 'Thành công: mật khẩu mới đã được gửi đến email của bạn.';
+$_['text_success']    = 'Thành công: bạn vui lòng kiểm tra email để thực hiện đổi mật khẩu.';
 
 // Entry
 $_['entry_email']     = 'Địa chỉ E-Mail:';
