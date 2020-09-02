a0b05fa98489c2f976c8bf1755e94dc591fc7a03
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-09-02 12:02:28 +0700

    add code to fix address 3

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Version
 define('VERSION', '3.0.2.0');
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Configuration
 if (is_file('config.php')) {
