98fcae9649ecfb1ddb4177c4fcf2d301039f22b4
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-09-02 12:12:53 +0700

    add code to fix address 11

--- catalog/controller/account/fix_account_error.php
+++ catalog/controller/account/fix_account_error.php
@@ -3,17 +3,16 @@ class ControllerAccountFixAccountError extends Controller {
 	public function index() {
 		$this->load->model('account/customer');
 
-		$error_customers = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer c LEFT JOIN " . DB_PREFIX . "address a ON (c.customer_id = a.customer_id) WHERE a.address_id IS NULL");
+		$error_customers = $this->db->query("SELECT c.customer_id AS customer_id, c.email AS email, c.firstname AS firstname, c.lastname AS lastname FROM " . DB_PREFIX . "customer c LEFT JOIN " . DB_PREFIX . "address a ON (c.customer_id = a.customer_id) WHERE a.address_id IS NULL");
 
 		if($error_customers->num_rows) {
 			foreach ($error_customers->rows as $customer) {
-				var_dump($customer);
 				echo "Fixing customer ID " . $customer['customer_id'] . " with email: " . $customer['email'] . "....<br/>";
 
 				$addresses = array();
 				$address = array();
-		        $address['firstname'] = $this->request->post['firstname'];
-		        $address['lastname'] = $this->request->post['lastname'];
+		        $address['firstname'] = $customer['firstname'];
+		        $address['lastname'] = $customer['lastname'];
 		        $address['company'] = 'NOT PROVIDED';
 		        $address['address_1'] = 'NOT PROVIDED';
 		        $address['address_2'] = 'NOT PROVIDED';
