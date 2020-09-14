e92dc506bca2de0bf22343a93e3481b30b08877d
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2020-09-14 22:43:46 +0700

    fix unicode name

--- catalog/controller/extension/module/ocproduct.php
+++ catalog/controller/extension/module/ocproduct.php
@@ -186,7 +186,7 @@ class ControllerExtensionModuleOcProduct extends Controller {
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
-					'name'        => $result['name'],
+					'name'        => trim(strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))),
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
